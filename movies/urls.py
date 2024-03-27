46dff5e8ddd6078979b48905a529c69787fcf455
Seline Cornfield <Seline2107@googlemail.com>
2024-03-27 14:02:21 +0000

    Referncing the URLs

--- movies/urls.py
+++ movies/urls.py
@@ -4,8 +4,10 @@ from .import views
 # define path object that maps url endpoints to view functions
 # this is the URL configuartion
 
+
+app_name = 'movies'
 urlpatterns = [
     # empty string represents the root of the app
-    path('', views.index, name='movies_index'),
-    path('<int:movie_id>', views.detail, name='movies_detail')
+    path('', views.index, name='index'),
+    path('<int:movie_id>', views.detail, name='detail')
 ]
