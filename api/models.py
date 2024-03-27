62e81f4398b5ec6097e03c28675a5e8060b0e7cd
Seline Cornfield <Seline2107@googlemail.com>
2024-03-27 14:50:58 +0000

    Build APIs

---
+++ api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from tastypie.resources import ModelResource
+from movies.models import Movie
+
+# Create your models/resources here. Representational State Transfer
+# To represent the concept of the movie in RESTful APIs
+
+
+class MovieResource(ModelResource):
+    class Meta:
+        queryset = Movie.objects.all()
+        resource_name = 'movies'
+        excludes = ['date_created']
